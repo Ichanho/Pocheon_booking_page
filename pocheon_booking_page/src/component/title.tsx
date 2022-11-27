a8e5255ebac6501e085d190aa07f7e7a2c4a4347
LEE CHAN HO <supprpass15@gmail.com>
2022-11-27 23:35:13 +0900

    Add CSS code

--- pocheon_booking_page/src/component/title.tsx
+++ pocheon_booking_page/src/component/title.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom"
+import styles from "./title.module.css"
 
 function Title() {
-  return <div>
+  return <div className={styles.title}>
     <div>
       <h1><Link to={"/"} style={{color: "inherit", textDecoration: "inherit"}}>포천 집 예약하기</Link></h1>
     </div>
-    <button>예약</button>
+    <button>예 약</button>
   </div>;
 }
 
