d7ce87ab0b642c48a5ceae95ed7609909e7b7f43
LEE CHAN HO <supprpass15@gmail.com>
2022-12-07 23:32:08 +0900

    Feat booking system
    Save reservation in json db.
    It has problem with DB.
    It have to fix everything. (How to show calender, How to save reservation...)

--- pocheon_booking_page/src/routes/Book.tsx
+++ pocheon_booking_page/src/routes/Book.tsx
@@ -1,29 +1,55 @@
-import { SyntheticEvent, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useRef, SyntheticEvent, useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import styles from "./Book.module.css"
 
 function Book() {
-
   function handleSubmit(event: SyntheticEvent) {
     event.preventDefault();
-    console.log(event.target);
-  }
 
+    if (checkInRef.current && checkOutRef.current && nameRef.current && passwordRef.current) {
+
+      const day = checkInRef.current.value;
+      const name = nameRef.current.value;
+      const pwd = passwordRef.current.value;
+
+      fetch("http://localhost:3001/booking/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          day: day,
+          name: name,
+          password: pwd
+      })
+      }).then((res)=>{
+        if(res.ok){
+          alert("예약이 완료되었습니다.");
+          navigate("/");
+        }
+      })
+    }
+  }
+  
   const [checkInDate, setcheckInDate] = useState(useParams().date);
   const [checkOutDate, setCheckOutDate] = useState(checkInDate);
 
+  const checkInRef = useRef<HTMLInputElement>(null);
+  const checkOutRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   return <div className={styles.book}>
-    {/* <p>{checkInDate}</p> */}
     <form action="" onSubmit={handleSubmit} className={styles.bookingForm}>
       <p>Check In (체크인)</p>
-      <input className={styles.bookingInput} type="date" id="checkIn" value={checkInDate} onChange={(e) => { setcheckInDate(e.target.value); console.log(e.target.value); }}></input>
+      <input className={styles.bookingInput} type="date" id="checkIn" value={checkInDate} onChange={(e) => { setcheckInDate(e.target.value); }} ref={checkInRef}></input>
       <p>Check Out (체크아웃)</p>
-      <input className={styles.bookingInput} type="date" value={checkOutDate} min={checkInDate} onChange={(e) => { setCheckOutDate(e.target.value) }}></input>
+      <input className={styles.bookingInput} type="date" value={checkOutDate} min={checkInDate} onChange={(e) => { setCheckOutDate(e.target.value) }} ref={checkOutRef}></input>
       <br />
-      <input className={styles.bookingInput} placeholder="사용자 이름" required></input>
+      <input className={styles.bookingInput} placeholder="사용자 이름" required ref={nameRef}></input>
       <br />
-      <input className={styles.bookingInput} placeholder="비밀번호" required></input>
+      <input className={styles.bookingInput} placeholder="비밀번호" required ref={passwordRef}></input>
       <br />
       <button>예약하기</button>
     </form>
